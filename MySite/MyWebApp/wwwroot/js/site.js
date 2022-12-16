5e78c81eef668cb0b9ac1fe73e583ff2e21e7fe0
Luiz Araujo <luizzz.freire@gmail.com>
2022-12-15 21:54:25 -0500

    Simplication of project.

---
+++ MySite/MyWebApp/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var typed = new Typed(".auto-type", {
+    strings: ["I'm Luiz Araujo", "I'm always learning", "I'm always improving", "How can I help you?"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
