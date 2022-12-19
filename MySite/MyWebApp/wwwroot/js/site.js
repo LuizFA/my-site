bd3a64180a4814739dcec669a8fc8a5bc5d7178e
Luiz Araujo <luizzz.freire@gmail.com>
2022-12-18 22:02:56 -0500

    more tests with typed js

--- MySite/MyWebApp/wwwroot/js/site.js
+++ MySite/MyWebApp/wwwroot/js/site.js
@@ -4,8 +4,17 @@
 // Write your JavaScript code.
 
 var typed = new Typed(".auto-type", {
-    strings: ["I'm Luiz Araujo", "I'm always learning", "I'm always improving", "How can I help you?"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
+    strings: ["I'm Luiz Antonio Freire Araujo", "I'm a full stack developer based on Quebec/CA", "I'm remotely working with C#.net for a Montreal's ecommerce company", "How can I help you?"],
+    typeSpeed: 75,
+    backSpeed: 50,
+    cursorChar: '_',
+    loop: false,
+    onComplete: (self) => {
+        self.strings = "I'm a full stack developer based on Quebec/CA, currently working for a Montreal's ecommerce company.<br>How can I help you?",
+        self.onComplete += (instance) => {
+                instance.cursor.remove();
+            }
+        //todo:restart here after loop complete
+        self.start();
+        }
+    })
